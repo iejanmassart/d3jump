bd7010fd05b2a3783be5f8370fb7c51a7a449386
iejanmassart <iejan.massart@ordina.be>
2022-03-28 16:04:16 +0200

    config object + implimentation + node mouseover fix

---
+++ config.js
@@ -0,0 +1,59 @@
+function configJP(conf) {
+  const myObj = new Object();
+  if (!conf.colorDots) {
+    myObj.colorDots = "orange";
+  } else if (conf.colorDots != "") {
+    myObj.colorDots = conf.colorDots;
+  }
+  if (!conf.width) {
+    myObj.width = 667;
+  } else {
+    myObj.width = conf.width;
+  }
+  if (!conf.height) {
+    myObj.height = 375;
+  } else {
+    myObj.height = conf.height;
+  }
+  if (!conf.hopColor) {
+    myObj.hopColor = "grey";
+  } else {
+    myObj.hopColor = conf.hopColor;
+  }
+  if (!conf.hopWidth) {
+    myObj.hopWidth = "1.5px";
+  } else {
+    myObj.hopWidth = conf.hopWidth;
+  }
+  if (!conf.hopBigWidth) {
+    myObj.hopBigWidth = "10px";
+  } else {
+    myObj.hopBigWidth = conf.hopBigWidth;
+  }
+  if (!conf.textAxisColor) {
+    myObj.textAxisColor = "grey";
+  } else {
+    myObj.textAxisColor = conf.textAxisColor;
+  }
+  if (!conf.axisColor) {
+    myObj.axisColor = "#000";
+  } else {
+    myObj.axisColor = conf.axisColor;
+  }
+  if (!conf.axisInfoColor) {
+    myObj.axisInfoColor = "orange";
+  } else {
+    myObj.axisInfoColor = conf.axisInfoColor;
+  }
+  if (!conf.transitionDuration) {
+    myObj.transitionDuration = 1000;
+  } else {
+    myObj.transitionDuration = conf.transitionDuration;
+  }
+  if (!conf.mouseOverColor) {
+    myObj.mouseOverColor = "";
+  } else {
+    myObj.mouseOverColor = conf.mouseOverColor;
+  }
+  return myObj;
+}
